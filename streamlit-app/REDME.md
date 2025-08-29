41080e2f8c53bfbead03add79888e5a227c63ef2
Desty Mpassi <desty.mpassi@gmail.com>
2025-08-29 12:12:40 +0200

    update requirements, page0, page3, readme

---
+++ streamlit-app/REDME.md
@@ -0,0 +1,52 @@
+# 📊 Tableau de bord Excel – Suivi des ventes
+
+## 📝 Contexte
+J’ai réalisé ce projet dans le cadre du notebook **“12 Projets pour devenir Data Analyst”** proposé par Natacha N. 
+L’objectif était de créer un **tableau de bord interactif sous Excel** pour suivre les performances commerciales d’une entreprise.
+
+## 📈 Aperçu visuel
+![Apercu visuel](./png)
+
+## 🎯 Objectif
+Créer un **dashboard Excel** clair et interactif permettant de :
+- Suivre les **ventes** et **marges** globales
+- Identifier les **régions ou catégories sous-performantes**
+- Permettre une **analyse dynamique** grâce à des filtres et segments
+
+## 📂 Base de Données
+[Superstore Dataset](https://www.kaggle.com/datasets/vivek468/superstore-dataset-final)
+
+
+## ⚙️ Étapes réalisées
+1. **Nettoyage & préparation des données**  
+   - Suppression des doublons, traitement des valeurs manquantes  
+   - Création de colonnes calculées (CA, Marge, etc.)
+
+2. **Analyse & traitement dans Excel**  
+   - Utilisation des formules : `SOMME.SI`, `MOYENNE`, `RECHERCHEV`, `INDEX`, `EQUIV`, etc.
+   - Création de **tableaux croisés dynamiques**  
+   - Ajout de **segments** pour filtrage dynamique
+
+3. **Création du tableau de bord**  
+   - Graphiques dynamiques (colonnes, barres, secteurs)  
+   - Organisation claire des KPIs  
+   - Interactivité pour faciliter la prise de décision
+
+
+## 🧠 Compétences développées
+- Maîtrise des **TCD (Tableaux Croisés Dynamiques)**  
+- Automatisation via **formules Excel avancées**  
+- Construction de **dashboards interactifs**  
+- Nettoyage et organisation de données
+
+
+## 🚀 Pour aller plus loin
+Ce projet peut être transposé sous **Power BI**, **Looker Studio**.
+
+
+## 👥​ Collaboration
+Projet réalisé en binôme avec [Desty MPASSI](https://github.com/DestyM)
+
+
+## 👨‍💻​ Auteur
+[Alberto BONGUELE](https://github.com/GitAlberto)
---
+++ streamlit-app/page0.py
@@ -0,0 +1,85 @@
+import streamlit as st
+
+# Interface utilisateur avec Streamlit
+st.set_page_config(
+    layout = "wide",
+    page_title = "MovieLens Data Analysis",
+    page_icon = "🎬"  # Emoji Unicode directement
+)
+
+# Conteneur pour aligner les éléments horizontalement
+col1, col2, col3 = st.columns([1, 4, 1])
+
+# Colonne gauche : Image
+with col1:
+    st.image(
+        "https://media.licdn.com/dms/image/v2/C4D03AQHBakAcLxLASQ/profile-displayphoto-shrink_400_400/profile-displayphoto-shrink_400_400/0/1635349619392?e=1752710400&v=beta&t=PnWBdipi1fnnssiEJ0CRtobrhh0M16-EGvwgWlDm7Nc",
+        width = 80,     # Ajustez la taille si nécessaire
+        use_container_width = False,
+    )
+
+# Colonne centrale : Titre
+with col2:
+    st.markdown(
+        """
+        <h1 style='text-align: center; margin-bottom: 0;'>Plateforme de recommandation de films (MovieLens)</h1>
+        """,
+        unsafe_allow_html = True,
+    )
+
+# Colonne droite : Nom et lien LinkedIn
+with col3:
+    st.markdown(
+        """
+        <div style='text-align: right;'>
+            <a href="https://www.linkedin.com/in/desty-mpassi-matondo/" target="_blank" style='text-decoration: none; color: #0077b5;'>
+                <strong>Desty MPASSI MATONDO</strong>
+            </a>
+        </div>
+        """,
+        unsafe_allow_html = True,
+    )
+
+st.write(" ")
+st.write(" ")
+
+# Titre
+st.markdown("# **Phase 1 : Développeur Python & Architecte API**")
+
+# Afficher l'image séparément
+st.image(
+        "./img/archi_api.png",
+         use_container_width=True
+        )
+
+st.markdown(
+        """
+        <a href="https://github.com/DestyM/movie-backend" target="_blank">
+            <button style="background-color: #28a745; color: white; padding: 10px 20px; border: none; border-radius: 8px; font-size: 16px;">
+                📦 Voir le code de la Phase 1
+            </button>
+        </a>
+        """,
+        unsafe_allow_html=True
+    )
+
+st.write(" ")
+st.write(" ")
+st.write(" ")
+
+
+# Titre
+st.markdown("# **Phase 2 : Data Analyst - Exploration et Visualisation**")
+# Afficher l'image séparément
+st.image("./img/archi_app.png", use_container_width=True)
+
+st.markdown(
+        """
+        <a href="https://github.com/DestyM/movie-analytics" target="_blank">
+            <button style="background-color: #28a745; color: white; padding: 10px 20px; border: none; border-radius: 8px; font-size: 16px;">
+                📊 Voir le code de la Phase 2
+            </button>
+        </a>
+        """,
+        unsafe_allow_html=True
+    )
